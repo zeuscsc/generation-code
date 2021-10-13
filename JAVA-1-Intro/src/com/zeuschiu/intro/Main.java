72d5cd3c5a764ddde11786e2dc5ccfde86987b9c
Zeus Chiu <zeuscsc@gmail.com>
2021-10-13 12:21:20 +0800

    java intro

--- JAVA-1-Intro/src/com/zeuschiu/intro/Main.java
+++ JAVA-1-Intro/src/com/zeuschiu/intro/Main.java
@@ -1,8 +1,20 @@
 package com.zeuschiu.intro;
 
 public class Main {
+    //Comment
+    /*Comment
+    * Ya
+    * */
+    /**This is the main test function
+     * */
+    public static void Test(){
 
+    }
     public static void main(String[] args) {
-	// write your code here
+        int a=0;
+        int b=1;
+        int c=a+b;
+        Main.Test();
+        System.out.println("Hello World");
     }
 }
