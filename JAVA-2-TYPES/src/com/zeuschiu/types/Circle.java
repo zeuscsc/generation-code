e5225ce904563d5c5be76423efb7642b26e66a24
Zeus Chiu <zeuscsc@gmail.com>
2021-10-14 11:55:57 +0800

    circle types

--- JAVA-2-TYPES/src/com/zeuschiu/types/Circle.java
+++ JAVA-2-TYPES/src/com/zeuschiu/types/Circle.java
@@ -12,11 +12,11 @@ public class Circle {
         return (double)diameter/2.0;
     }
     public float area(){
-        return (float)(Math.pow(radius(),2.0)*PI);
+        return (float) (Math.pow(radius(),2.0)*PI);
     }
     @Override
     public String toString(){
-        return String.format("Circle %s having diameter of %f and its area is %f",
-                super.toString(),diameter,area());
+        return String.format("Circle %s having diameter of %d and its area is %.2f",
+                super.hashCode(),(int)diameter,area());
     }
 }
